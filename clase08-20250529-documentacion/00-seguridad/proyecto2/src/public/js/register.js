c21c2e82df985299ca6b8bd93d77e6da64442479
Diego Polverelli <diegopolverelli@hotmail.com>
2025-05-29 21:13:30 -0300

    clase 08 added

---
+++ clase08-20250529-documentacion/00-seguridad/proyecto2/src/public/js/register.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('registerForm');
+
+form.addEventListener('submit',evt=>{
+    evt.preventDefault();
+    const data = new FormData(form);
+    const obj = {}
+    data.forEach((value, key)=> obj[key]=value);
+    fetch('/api/sessions/register',{
+        method:'POST',
+        body:JSON.stringify(obj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(result=>{
+        if(result.status>=400) throw new Error(result.statusText)
+        return result.json()
+    }).then(json=>{
+        Swal.fire({
+            icon:"success",
+            text:"Usuario registrado :)"
+        })
+    }).catch(error=>{
+        console.log(error)
+        alert(`Error: ${error.message}`)
+    })
+})
